7fca585f30066f277549ce6cf027dbdb75673c85
veronika <veronika.kri151@gmail.com>
2017-01-23 19:57:27 +0200

    added modularity

--- client.py
+++ client.py
@@ -4,9 +4,15 @@ import socket
 client_socket = socket.socket()
 client_socket.connect(("127.0.0.1", 1337))
 
-data = raw_input()
-client_socket.send(data)
-server_data = client_socket.recv(1337)
-print "server's response:" +  server_data
+get_send_data()
+get_print_res()
+
+def get_send_data():
+	data = raw_input()
+	client_socket.send(data)
+
+def get_print_res():
+	server_data = client_socket.recv(1337)
+	print "server's response:" +  server_data
 
 client_socket.close()
--- server.py
+++ server.py
@@ -5,8 +5,17 @@ server_socket = socket.socket()
 server_socket.bind(("0.0.0.0", 1337))
 
 while True:
+	listen()
+	client_connect()
+	recive_send_data()
+
+def listen():
 	server_socket.listen(1)
+
+def client_connect():
 	(client_socket, client_address) = server_socket.accept()
+
+def recive_send_data():
 	client_msg = client_socket.recv(1024)
 	client_socket.send(client_msg)
 
