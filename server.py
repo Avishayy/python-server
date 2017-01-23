af4ea790861b698e9b126b8031d1399888aeb0c0
veronika <veronika.kri151@gmail.com>
2017-01-23 19:31:01 +0200

    simple server added

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+
+
+server_socket = socket.socket()
+server_socket.bind((‘0.0.0.0’, 1337))
+
+while true:
+	server_socket.listen(1)
+	(client_socket, client_address) = server_socket.accept()
+
+client_msg = client_socket.recv(1024)
+client_socket.send(client_msg)
