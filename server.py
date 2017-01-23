8253bd6988b437b71095a291e11f813eb9ea9330
veronika <veronika.kri151@gmail.com>
2017-01-23 19:51:46 +0200

    fixed stupid mistakes

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ import socket
 
 
 server_socket = socket.socket()
-server_socket.bind((‘0.0.0.0’, 1337))
+server_socket.bind(("0.0.0.0", 1337))
 
-while true:
+while True:
 	server_socket.listen(1)
 	(client_socket, client_address) = server_socket.accept()
+	client_msg = client_socket.recv(1024)
+	client_socket.send(client_msg)
 
-client_msg = client_socket.recv(1024)
-client_socket.send(client_msg)
 
 client_socket.close()
 server_socket.close()
